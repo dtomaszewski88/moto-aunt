b575a1c76b54c4e1f7d1437779584cfd2f5741b2
dawid <dtomaszewski88@gmail.com>
2023-01-22 11:36:54 +0100

    minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,26 +5,22 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
-  purge: {
-    options: {
-      safelist: [
-        'bg-red-600',
-        'bg-orange-500',
-        'bg-amber-500',
-        'bg-yellow-300',
-        'bg-lime-200',
-        'bg-green-500',
-        'bg-emerald-500',
-        'bg-teal-500',
-        'bg-blue-900',
-        'bg-indigo-500',
-        'bg-violet-500',
-        'bg-purple-500',
-        'bg-fuchsia-500',
-        'bg-rose-500'
-      ]
-    }
-  },
+  safelist: [
+    'bg-red-600',
+    'bg-orange-500',
+    'bg-amber-500',
+    'bg-yellow-300',
+    'bg-lime-200',
+    'bg-green-500',
+    'bg-emerald-500',
+    'bg-teal-500',
+    'bg-blue-900',
+    'bg-indigo-500',
+    'bg-violet-500',
+    'bg-purple-500',
+    'bg-fuchsia-500',
+    'bg-rose-500'
+  ],
   theme: {
     extend: {}
   },
